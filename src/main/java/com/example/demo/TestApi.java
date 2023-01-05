ad70e2417ff128716d0da3fef23997df1603e1a3
moshenglu <youming@aliyun.com>
2023-01-05 11:40:53 +0800

    update

--- src/main/java/com/example/demo/TestApi.java
+++ src/main/java/com/example/demo/TestApi.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class TestApi {
+
+    private static final int MIN_VALUE = 100;
+    private static final int MAX_VALUE = 500;
     @PostMapping(value = "/testApi")
-    public ResponseEntity<Object> testApi(){
+    public ResponseEntity<Object> testApi() throws InterruptedException {
+
+        Thread.sleep((long)Math.random()*(MAX_VALUE-MIN_VALUE)+MIN_VALUE);
         Map<String,String> map = new HashMap<>();
         return ResponseEntity.status(HttpStatus.OK).body(map);
     }
