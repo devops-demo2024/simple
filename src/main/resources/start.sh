46e12c63ed45e43c1b0ae391d54008ecceeee008
moshenglu <youming@aliyun.com>
2023-01-13 17:57:45 +0800

    update

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/app/apps/demo
+if [ -d  /home/app/apps/demo ]; then
+  cd /home/app/apps/demo
+fi
+
 nohup java -Xms256m -Xmx512m -Dapp=demo org.springframework.boot.loader.JarLauncher  > demo.log 2>&1 &
