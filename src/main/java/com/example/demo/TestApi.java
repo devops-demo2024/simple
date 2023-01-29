be70164ae6db3722d564410aa5e899abcc03b241
moshenglu <youming@aliyun.com>
2023-01-29 18:50:34 +0800

    update to 200

--- src/main/java/com/example/demo/TestApi.java
+++ src/main/java/com/example/demo/TestApi.java
@@ -12,7 +12,7 @@ import java.util.Random;
 @RestController
 public class TestApi {
 
-    private static final int MIN_VALUE = 250;
+    private static final int MIN_VALUE = 200;
     private static final int MAX_VALUE = 500;
     @PostMapping(value = "/testApi")
     public ResponseEntity<Object> testApi() throws InterruptedException {
